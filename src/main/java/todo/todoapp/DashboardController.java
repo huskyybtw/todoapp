f086e2c0e548f7f4870e2107cf8f74bba3c9ea76
hania <hania911t5@gmail.com>
2023-11-15 13:33:32 +0100

    fixed login button from LogInScene to DashboardScene

--- src/main/java/todo/todoapp/DashboardController.java
+++ src/main/java/todo/todoapp/DashboardController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,7 +17,9 @@ public class DashboardController {
     private Scene scene;
     private Parent root;
     @FXML
-    public void switchToLogIn(ActionEvent event) throws IOException {
+    private Button r_login;
+    @FXML
+    public void switchToLogIn1(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LogInScene.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
--- src/main/java/todo/todoapp/LogInController.java
+++ src/main/java/todo/todoapp/LogInController.java
@@ -24,4 +24,12 @@ public class LogInController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchToDashboard(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("DashboardScene.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
