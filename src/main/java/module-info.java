a548c371f5372fad33df6dc77e367565aa962e2d
huskybtw <marekkocikw@gmail.com>
2023-11-15 13:13:35 +0100

    fixed connection string to module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module todo.todoapp {
     requires javafx.fxml;
     requires org.mongodb.bson;
     requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
 
 
     opens todo.todoapp to javafx.fxml;
