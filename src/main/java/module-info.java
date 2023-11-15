d27f70d6eadb89022750cb97a64b6ba8b5ec5a36
huskybtw <marekkocikw@gmail.com>
2023-11-15 11:58:47 +0100

    added dependency (javadriver) updated module Linked to db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module todo.todoapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.sync.client;
 
 
     opens todo.todoapp to javafx.fxml;
--- src/main/java/todo/todoapp/MongoDB.java
+++ src/main/java/todo/todoapp/MongoDB.java
@@ -1,12 +1,17 @@
 package todo.todoapp;
 
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoDB {
 
-    private final connectionString = "mongodb+srv://admin:admin@cluster0.iou5rdl.mongodb.net/?retryWrites=true&w=majority";
-    private final mongoClient = MongoClients.create(connectionString);
-    private final database = mongoClient.getDatabase("test");
-    MongoCollection<Document> W_collection = database.getCollection("Worker");
-    MongoCollection<Document> S_collection = database.getCollection("Supervisor");
-    MongoCollection<Document> A_collection = database.getCollection("Admin");
-
+    private final String URI = "mongodb+srv://admin:admin@cluster0.iou5rdl.mongodb.net/?retryWrites=true&w=majority";
+    private final MongoClient MONGOCLIENT = MongoClients.create(URI);
+    private final MongoDatabase DATABASE = MONGOCLIENT.getDatabase("test");
+    private MongoCollection<Document> W_COLLECTION = DATABASE.getCollection("Worker");
+    private MongoCollection<Document> S_COLLECTION = DATABASE.getCollection("Supervisor");
+    private MongoCollection<Document> A_COLLECTION = DATABASE.getCollection("Admin");
 }
