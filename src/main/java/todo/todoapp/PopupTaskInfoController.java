6e2e4ee1faa9f71966f4e34d862741f136c09af3
huskybtw <marekkocikw@gmail.com>
2023-11-24 15:35:01 +0100

    NOW U CAN CREATE DEADLINES AND TASK DESCRIPTIONS

--- src/main/java/todo/todoapp/PopupTaskInfoController.java
+++ src/main/java/todo/todoapp/PopupTaskInfoController.java
@@ -5,15 +5,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import todo.todoapp.General.Assignment;
 
+import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class PopupTaskInfoController {
 
     @FXML
     Label TITLE_Label,CREATED_BY_Label,CREATION_DATE_Label,deadline_Label,status_Label;
+    @FXML
+    TextArea description_field;
     @FXML
     ListView<String> assigned_users_ListView;
 
@@ -23,12 +29,12 @@ public class PopupTaskInfoController {
     public void initialize(Assignment t,DashboardController d){
         task = t;
         dashboardController = d;
-
+        System.out.println(t.getDeadline());
         // FORMAT DATES INTO STRINGS
-        Date creationDate = task.getCREATION_DATE();
-        Date deadlineDate = task.getDeadline();
+        LocalDateTime creationDate = task.getCREATION_DATE();
+        LocalDateTime deadlineDate = task.getDeadline();
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Adjust the pattern based on your requirements
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // Adjust the pattern based on your requirements
 
         String temp_cdate = dateFormat.format(creationDate);
         String temp_ddate = dateFormat.format(deadlineDate);
@@ -42,11 +48,14 @@ public class PopupTaskInfoController {
         deadline_Label.setText("deadline : " + temp_ddate);
         status_Label.setText("status " + temp_status);
 
+        description_field.setText(task.getDescription());
+
         // ASSIGNED USERS LISTVIEW
             // Convert ArrayList to ObservableList
         ObservableList<String> observableList = FXCollections.observableList(task.getAssigned_users());
         // Set the ObservableList as the items for the ListView
         assigned_users_ListView.getItems().setAll(observableList);
     }
+
 
 }
